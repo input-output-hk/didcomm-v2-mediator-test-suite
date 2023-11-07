a6288ef6536cd15181c30896841f5c33ef0c050b
Anton Baliasnikov <Anton.baliasnikov@iohk.io>
2023-11-07 23:07:45 +0000

    fix: use dejavu in report problem for repeated message (#10)

--- src/test/kotlin/common/ReportProblemErrors.kt
+++ src/test/kotlin/common/ReportProblemErrors.kt
@@ -2,6 +2,7 @@ package common
 
 object ReportProblemErrors {
     val STORAGE = "e.p.me.res.storage"
+    val DEJAVU = "e.p.crypto.message.dejavu"
     val UNSUPPORTED_PROTOCOL = "e.p.msg.unsupported"
     val ERROR = "e.p.error"
     val NOT_ENROLL = "e.p.req.not_enroll"
